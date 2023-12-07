2c3b16fb92e19236d9c6dd9f6b1e12037431dc97
Jo <joseph.gayed91@gmail.com>
2023-12-07 19:42:13 +0200

    Firebase App Distribution Android: Automate the process of building and uploading apk to firebase app distribution.
    To use it:
    1- Update the version name , code in AppVersions class.
    2- Update the release_notes.txt
    3- Execute the command "./apk_app_distribution.sh" in terminal.

---
+++ app_distribution/apk_app_distribution.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the firebase config variables
+apkPath="../build/app/outputs/flutter-apk/app-release.apk"
+androidAppId="1:974856763223:android:aff93845402a46edc37012"
+groupsNames="Dev,QC,Business"
+releaseNotes=$(<release_notes.txt)
+
+
+# Your Flutter build command (e.g., Android APK)
+flutter build apk --release
+
+# Upload APK to App distribution 
+firebase appdistribution:distribute "$apkPath" --app  "$androidAppId" --release-notes "$releaseNotes" --groups "$groupsNames"
---
+++ app_distribution/app_version.dart
@@ -0,0 +1,4 @@
+class AppVersion {
+  static const String versionName = '0.1.0';
+  static const int versionCode = 1;
+}
