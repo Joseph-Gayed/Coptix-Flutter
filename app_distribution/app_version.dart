fa2ff2c314c093099d8077fd1b17111f85ab50e7
Jo <joseph.gayed91@gmail.com>
2023-12-10 12:26:09 +0200

    build 0.2.0: fix comments of build 0.1.0 and adding tabs (episodes,related) in details screen.

--- app_distribution/app_version.dart
+++ app_distribution/app_version.dart
@@ -1,4 +1,4 @@
 class AppVersion {
-  static const String versionName = '0.1.0';
-  static const int versionCode = 1;
+  static const String versionName = '0.2.0';
+  static const int versionCode = 2;
 }
