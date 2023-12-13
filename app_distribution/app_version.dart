8386ac3c59af6db3a1e0e716514ebf7578d7ec53
Jo <joseph.gayed91@gmail.com>
2023-12-13 18:30:12 +0200

    build 0.3.0: fix Ui comments of build 0.2.0 and adding Special collection , Categories chips in home screen.

--- app_distribution/app_version.dart
+++ app_distribution/app_version.dart
@@ -1,4 +1,4 @@
 class AppVersion {
-  static const String versionName = '0.2.0';
-  static const int versionCode = 2;
+  static const String versionName = '0.3.0';
+  static const int versionCode = 3;
 }
